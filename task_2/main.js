c2982b4fdbcdd6b8c416e6d61387439e620b5708
Тищенко Виталий Викторович <tishenko@programist.ru>
2019-03-11 23:12:25 +0300

    modify task 2

--- task_2/main.js
+++ task_2/main.js
@@ -28,3 +28,22 @@ console.log(x++);
 
 let conf = [ ] + false - null + true;
 console.log(conf);
+
+let y = 1; let x = y = 2; console.log(x);
+
+let summ =  [] + 1 + 2;
+console.log(summ);
+
+console.log( "1"[0] );
+console.log(2 && 1 && null && 0 && undefined);
+
+console.log( null || 2 && 3 || 4 );
+
+a = [1, 2, 3]; b = [1, 2, 3];
+console.log(a == b);
+
+console.log( +"Infinity" ); 
+
+console.log("Ёжик" > "яблоко");
+
+console.log(0 || "" || 2 || undefined || true || falsе);
